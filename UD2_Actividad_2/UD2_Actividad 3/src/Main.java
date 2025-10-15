d30b27badea9748b8bdd65d9ab937bad1b05ea43
COMPU-S026\daw1 <jdesouzar01@educantabria.es>
2025-10-15 09:53:56 +0200

    UD2_Actividad 3

--- UD2_Actividad_2/UD2_Actividad 3/src/Main.java
+++ UD2_Actividad_2/UD2_Actividad 3/src/Main.java
@@ -9,5 +9,22 @@ public class Main {
         //Ejercicio 1
         System.out.println("Ejercicio 1");
 
+        System.out.println("Introduce una cantidad multiplo de 5: ");
+        int cantidad = sc.nextInt();
+
+        if (cantidad % 5 != 0){
+            System.out.println("La cantidad debe ser multiplo de 5.");
+        } else {
+            int [] billetes = {500, 200, 100,50, 20, 10, 5};
+            int [] cantidadBilletes = new int[billetes.length];
+
+            int resto = cantidad;
+
+            for (int i = 0; i < billetes.length; i++);
+            {
+                cantidadBilletes[i] = resto / billetes[i];
+                resto = resto % billetes[i];
+            }
+        }
     }
 }
