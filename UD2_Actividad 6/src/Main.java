7d26b4280e884a647df275059d2896f8fead5b73
COMPU-S026\daw1 <jdesouzar01@educantabria.es>
2025-11-03 09:47:44 +0100

    Actividades 5 y 6

--- UD2_Actividad 6/src/Main.java
+++ UD2_Actividad 6/src/Main.java
@@ -12,7 +12,7 @@ public class Main {
         int filas = 5;
         int columnas = 5;
 
-        int[][] matriz = new int[filas][columnas];
+        int[][] matriz1 = new int[filas][columnas];
 
         int contador = 1;
 
@@ -20,13 +20,13 @@ public class Main {
 
         for (int i = 0; i < filas; i++){
             for (int j = 0; j < columnas; j++){
-                matriz[i][j] = contador;
+                matriz1[i][j] = contador;
                 contador++;
             }
         }
         for (int i = 0; i < filas; i++){
             for (int j = 0; j < columnas; j++){
-                System.out.print(matriz[i][j]);
+                System.out.print(matriz1[i][j]);
 
             }
             System.out.println();
@@ -36,71 +36,66 @@ public class Main {
         System.out.println("Ejercicio 2");
         sc = new Scanner(System.in);
 
-        int filas1 = 11;
-        int columnas1 = 11;
+        int filas1 = 10;
+        int columnas1 = 10;
 
         int[][] tablas = new int[filas1][columnas1];
 
         int contador1 = 1;
 
-        System.out.println("---Tabla de Multiplicar del 1 al 10(Matriz 10x10)---");
-
         for (int i = 0; i < tablas.length; i++){
             for (int j = 0; j < tablas[i].length; j++){
-                tablas[i][j] = i * j;
-            }
-            System.out.printf("Tabla del %2d: |", + i);
-        }
-        for (int i = 0; i < tablas.length; i++){
-            for (int j = 0; j < tablas[i].length; j++){
+                tablas[i][j] = (i + 1) * (j + 1);
                 System.out.print(tablas[i][j] + " ");
+
             }
-            System.out.println();
-
-    }
-        //Ejercicio 3
-        System.out.println("Ejercicio 3");
-        sc = new Scanner(System.in);
-
-        int filas2 = 0;
-        int columnas2 = 0;
-        
-        int mayor = 0;
-        int menor = 0;
-        int igual = 0;
-
-        System.out.println("Introduzca el numero de filas: ");
-        filas2 = sc.nextInt();
-
-        System.out.println("Introduzca el numero de columnas: ");
-        columnas2 = sc.nextInt();
-
-        int[][] tablas2 = new int[filas2][columnas2];
-        
-        for (int i = 0; i < tablas2.length; i++){
-            for (int j = 0; j < tablas2[i].length; j++){
-                System.out.println("Introduzca el valor ["+i+"]"+"["+j+"]");
-                tablas2[i][j] = sc.nextInt();
-            }
+            System.out.print();
         }
 
-
-        for (int i = 0; i < tablas2.length; i++){
-            for (int j = 0; j < tablas2[i].length; j++){
-                
-                if(tablas2[i][j] > 0){
-                    mayor++;
-                } else if (tablas2[i][j] < 0) {
-                    menor++;
-                }else{
-                    igual++;
-                }
-            }
-        }
-
-        System.out.println("La cantidad de números que son mayor que cero es: " + mayor);
-        System.out.println("La cantidad de números que son menores que cero es: " + menor);
-        System.out.println("La cantidad de números que son iguales que cero es: " + igual);
+//    }
+//        //Ejercicio 3
+//        System.out.println("Ejercicio 3");
+//        sc = new Scanner(System.in);
+//
+//        int filas2 = 0;
+//        int columnas2 = 0;
+//
+//        int mayor = 0;
+//        int menor = 0;
+//        int igual = 0;
+//
+//        System.out.println("Introduzca el numero de filas: ");
+//        filas2 = sc.nextInt();
+//
+//        System.out.println("Introduzca el numero de columnas: ");
+//        columnas2 = sc.nextInt();
+//
+//        int[][] tablas2 = new int[filas2][columnas2];
+//
+//        for (int i = 0; i < tablas2.length; i++){
+//            for (int j = 0; j < tablas2[i].length; j++){
+//                System.out.println("Introduzca el valor ["+i+"]"+"["+j+"]");
+//                tablas2[i][j] = sc.nextInt();
+//            }
+//        }
+//
+//
+//        for (int i = 0; i < tablas2.length; i++){
+//            for (int j = 0; j < tablas2[i].length; j++){
+//
+//                if(tablas2[i][j] > 0){
+//                    mayor++;
+//                } else if (tablas2[i][j] < 0) {
+//                    menor++;
+//                }else{
+//                    igual++;
+//                }
+//            }
+//        }
+//
+//        System.out.println("La cantidad de números que son mayor que cero es: " + mayor);
+//        System.out.println("La cantidad de números que son menores que cero es: " + menor);
+//        System.out.println("La cantidad de números que son iguales que cero es: " + igual);
 
         //Ejercicio4
         System.out.println("Ejercicio 4");
@@ -113,6 +108,34 @@ public class Main {
 
         System.out.println("Introduzca las notas de 0 a 10");
 
-        for (int i = 0, i < )
+        for (int i = 0; i < alumnos; i++){
+            System.out.println("Alumno " + (i + 1) + " ==");
+
+            for (int j = 0; j < asignaturas; j++){
+                System.out.print("Introduce la nota para la asignatura " + (j + 1) + ": ";
+                notas[i][j] = sc.nextDouble();
+            }
+        }
+        for (int i = 0; i < alumnos; i++){
+            double suma = 0.0;
+            double notaMinima = notas[i][0];
+            double notaMaxima = notas[i][0];
+
+            for (int j = 0; j < asignaturas; j++){
+                suma = suma + notas[i][j];
+
+                if (notas[i][j] > notaMaxima){
+                    notaMaxima = notas[i][j];
+                }
+                if (notas[i][j] < notaMinima){
+                    notaMaxima = notas[i][j];
+                }
+            }
+            double media = suma / asignaturas;
+
+            System.out.println("La nota maxima: " + notaMaxima);
+
+        }
+
     }
 }
